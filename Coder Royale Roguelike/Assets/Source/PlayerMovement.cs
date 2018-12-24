5ffa90b7cdfa3e89e26f1b75ed944c4edd67a1fc
rosdyana <rosdyana.kusuma@gmail.com>
2018-12-25 02:45:09 +0800

    [+] add player movement animation

---
+++ Coder Royale Roguelike/Assets/Source/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private float movementSpeed;
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveInput = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0f);
+        animator.SetFloat("Horizontal", moveInput.x);
+        animator.SetFloat("Vertical", moveInput.y);
+        animator.SetFloat("Magnitude", moveInput.magnitude);
+        transform.position = transform.position + moveInput * Time.deltaTime * movementSpeed;
+    }
+
+}
